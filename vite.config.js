6c8e2048dc4d4ec7fd481d2aaa4e03e030b7dba4
khyahahati <khyati.tiwari.2003@gmail.com>
2026-03-08 00:55:37 +0530

    add build

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
+    base: "/connect-four/",
     plugins: [react()],
     server: {
         host: '0.0.0.0',
-        port: 5173,
-        proxy: {
-            '/leaderboard': 'http://localhost:8080',
-            '/ws': {
-                target: 'ws://localhost:8080',
-                ws: true
-            }
-        }
+        port: 5173
     }
 });
